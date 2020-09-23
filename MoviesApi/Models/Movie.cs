b22417674450c031a039745f15145e0e038eb8b7
Gareth Dobson <garethdobson@gmx.com>
2020-09-23 16:15:55 +0100

    Adding MoviesAPI solution files

---
+++ MoviesApi/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace MoviesApi.Models
+{
+    public class Movie
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Title")]
+        [JsonProperty("Title")]
+        public string MovieTitle { get; set; }
+
+        public string Director { get; set; }
+
+        public string Category { get; set; }
+
+        public string Rating { get; set; }
+    }
+}
